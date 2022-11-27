b721ba2eae5b53553e340458d9636d5580de2429
kris <kris.laermans@gmail.com>
2022-11-27 13:30:09 +0100

    Add update word feature, to be tested

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request
 from flask_swagger_ui import get_swaggerui_blueprint
 import sqlite3
 import uuid
@@ -30,7 +30,8 @@ def get_words(session_uuid):
     try:
         with sqlite3.connect(db) as con:
             con.row_factory = sqlite3.Row
-            words = con.execute('SELECT word.word as word, word.ordering as ordering, session.uuid as session_uuid '
+            words = con.execute('SELECT word.id as word_id, word.word as word, word.ordering as ordering, '
+                                'session.uuid as session_uuid '
                                 'FROM word LEFT JOIN session ON word.session_id = session.id '
                                 'WHERE session.uuid = ? ORDER BY word.ordering;', (session_uuid,)).fetchall()
             # words = con.execute('SELECT * FROM word').fetchall()
@@ -47,19 +48,66 @@ def get_words(session_uuid):
         con.close()
 
 
+@app.route("/g4m/api/v1/update_word/<session_uuid>/<creator_uuid>", methods=['POST'])
+def update_word(session_uuid, creator_uuid):
+    content_type = request.headers.get('Content-Type')
+    workflow = "add"
+    if content_type == 'application/json':
+        json = request.json
+    else:
+        return {'Content-Type not supported!'}, 400
+    if json['word_id'] is not None:
+        workflow = "update"
+    if json['word'] is None:
+        return {'Missing data word'}, 400
+    if json['ordering'] is None:
+        json['ordering'] = 0
+    try:
+        with sqlite3.connect(db) as con:
+            con.row_factory = sqlite3.Row
+            session = con.execute('SELECT session_id, creator_uuid FROM session WHERE session_uuid = ? LIMIT 1',
+                                  (session_uuid,)).fetchone()
+
+            if session['creator_uuid'] != creator_uuid:
+                return {'The creator token is not valid'}, 403
+
+            cur = con.cursor()
+            if workflow == "add":
+                cur.execute("INSERT INTO word (word,session_id,ordering) "
+                            "VALUES (?,?,?)",
+                            (json['word'], session['session_id'], json['ordering']))
+            con.commit()
+    except:
+        con.rollback()
+        msg = "error in insert operation"
+        return {msg}, 400
+
+    finally:
+        return {}, 201
+        con.close()
+    return ""
+
+
 @app.route("/g4m/api/v1/create_session", methods=['POST'])
 def create_session():
     session_uuid = str(uuid.uuid4())
     creator_uuid = str(uuid.uuid4())
     verifiers_uuid = str(uuid.uuid4())
+    content_type = request.headers.get('Content-Type')
+    type_id = 0
+    if content_type == 'application/json':
+        json = request.json
+    else:
+        return {'Content-Type not supported!'}, 400
+    if json['type'] == "all_words_together":
+        type_id = 1
     try:
         with sqlite3.connect(db) as con:
             cur = con.cursor()
             cur.execute("INSERT INTO session (uuid,type_id,domain_id,verifier_token, creator_token, expiration_date) "
                         "VALUES (?,?,?,?,?,date('now', '+3 days'))",
-                        (session_uuid, 1, 1, verifiers_uuid, creator_uuid))
+                        (session_uuid, type_id, 1, verifiers_uuid, creator_uuid))
             con.commit()
-            msg = "New session successfully stored"
     except:
         con.rollback()
         msg = "error in insert operation"
